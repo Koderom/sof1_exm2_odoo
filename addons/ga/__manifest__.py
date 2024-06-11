821ea3d391e33bc2fb80a339dfba0db7abbecd7a
matias <matias@matiass-iMac-Pro.local>
2024-06-10 21:40:24 -0700

    merge 2

--- addons/ga/__manifest__.py
+++ addons/ga/__manifest__.py
@@ -11,20 +11,26 @@
     'depends': ['base', 'hr'],
     'data': [
         'security/ir.model.access.csv',
-        'views/ga_inscripcion_views.xml',
         'data/ga.gestion.csv',
         'data/ga.nivel.csv',
         'data/ga.grado.csv',
         'data/ga.materia.csv',
         'data/ga.aula.csv',
         'data/ga.paralelo.csv',
-
         'views/ga_gestion_views.xml',
         'views/ga_unidad_educativa_views.xml',
         'views/ga_materia_views.xml',
         'views/ga_plan_estudio_views.xml',
-        'views/ga_menu_views.xml'
-       
+        #'views/ga_menu_views.xml'
+        'views/ga_menu_view.xml',
+        'views/ga_actividad_academica.xml',
+        'views/ga_alumno_view.xml',
+        'views/ga_apoderado_view.xml',
+        'views/ga_chofer_view.xml',
+        'views/ga_inscripcion_view.xml',
+        'views/ga_registro_academico.xml',
+        'views/ga_ruta_chofer_view.xml',
+        'views/ga_rutas_view.xml'
     ],
     'installable': True,
     'auto_install': False,
